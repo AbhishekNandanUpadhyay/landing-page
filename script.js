46e9760d296231ff1e39c8595f1cf8381363a053
Abhishek Nandan Upadhyay <91962442+AbhishekNandanUpadhyay@users.noreply.github.com>
2023-12-15 11:42:17 +0530

    Update scriptjs

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 Shery.imageEffect(".back", {style:5, config:{"a":{"value":1.49,"range":[0,30]},"b":{"value":-0.98,"range":[-1,1]},"aspect":{"value":1.9926701570680627},"gooey":{"value":true},"infiniteGooey":{"value":true},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.3,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":1},"noise_speed":{"value":1.4,"range":[0,10]},"metaball":{"value":0.17,"range":[0,2]},"discard_threshold":{"value":0.44,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.45,"range":[0,2]},"noise_scale":{"value":11.21,"range":[0,100]}}, gooey:true});
+function openVideoInNewTab() {
+    var videoUrl = "RayBan.mp4";
 
+    window.open(videoUrl, '_blank');
+  }
 
 var elems = document.querySelectorAll(".elem");
 
